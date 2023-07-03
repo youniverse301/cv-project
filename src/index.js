e05bea9cb475fd0a4ef036be9e6f796e9576ab3a
Bjorn <bjorneng3@gmail.com>
2023-07-03 02:38:27 -0400

    css changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/app.css';
-import './styles/personalDetails.css';
+import './styles/editContainer.css';
 import './styles/productContainer.css';
 
 
